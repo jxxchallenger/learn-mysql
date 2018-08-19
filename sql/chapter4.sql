6156521a46d064e28b14fd813e2c86a262b772c4
jxxchallenger <jxxchallenger@foxmail.com>
2018-08-19 20:48:09 +0800

    chapter 4

--- sql/chapter4.sql
+++ sql/chapter4.sql
@@ -38,3 +38,45 @@ VALUES
 ('Mr.', 'Richard', 'Stringer', 'richard@qq.com'),
 ('Ms.', 'Rusty', 'Osborne', 'rusty@qq.com'),
 ('Ms.', 'Lexi', 'Hollar', 'lexi@qq.com');
+
+CREATE TABLE IF NOT EXISTS rookery.bird_families (
+	family_id INT AUTO_INCREMENT PRIMARY KEY,
+  scientific_name VARCHAR(255) UNIQUE,
+  brief_description VARCHAR(255)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS rookery.bird_orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  scientific_name VARCHAR(255) UNIQUE,
+  brief_description VARCHAR(255),
+  order_image BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+DROP TABLE bird_orders;
+
+CREATE TABLE IF NOT EXISTS rookery.bird_orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  scientific_name VARCHAR(255) UNIQUE,
+  brief_description TEXT,
+  order_image BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS rookery.birds_wing_shapes (
+  wing_id CHAR(2) UNIQUE,
+  wing_shape CHAR(25),
+  wing_example BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+SHOW CREATE TABLE birds_wing_shapes;
+
+CREATE TABLE IF NOT EXISTS rookery.birds_body_shapes (
+  body_id CHAR(3) UNIQUE,
+  body_shape CHAR(25),
+  body_example BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS rookery.birds_bill_shapes (
+  bill_id CHAR(2) UNIQUE,
+  bill_shape CHAR(25),
+  bill_example BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
